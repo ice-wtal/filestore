d083672cf472fc6564291facaf687e16111525c5
Sven Anacker <sven@anacker.info>
2016-07-14 16:25:29 +0200

    First commit

---
+++ config.inc.php
@@ -0,0 +1,20 @@
+<?php
+
+/*
+	$_SESSION['cfg_param']			// Array des übergebenen Parameter param
+	$_SESSION['cfg_lastRefresh']	// Zeit des letzten Seitenaufrufes
+	$_SESSION['message'][]			// Array mit Nachrichten zum anzeigen ( nachricht::level )
+	$_SESSION['cfg_userno']			// User Nr., wenn > 0 ist User angemeldet
+*/
+
+	$_SESSION['cfg_base_url']		= "http://www.filestore.com/";
+	$_SESSION['cfg_rootdir']		= '/home/xxx/public_html/filestore'; // Verzeichnis der index.php (Startdatei)
+
+	$_SESSION['cfg_db_host'] = "localhost";
+	$_SESSION['cfg_db_name'] = "db_name";
+	$_SESSION['cfg_db_user'] = "username";
+	$_SESSION['cfg_db_pass'] = "xxxxxxxxxxxxxx";
+
+	$_SESSION['cfg_autologout'] = 10;		// Anzahl der Minuten bis automatischem Logout
+
+?>
---
+++ inc/file_op.inc.php
@@ -0,0 +1,103 @@
+<?php/*	File Operations	ToDo:	- Ordner files erstellen wenn nicht vorhanden*/class file_op {	private $mysqli;	private $folders;		// unterverzeichnisse von /files		function __construct( $mysqli ) {		$this->mysqli = $mysqli;
+	}		function body() { // wird im Hauptbereich angezeigt		//		print_r( $_POST );		echo '<div class="page-header">				<h1>Filelinks <small>to direct download, use in Mailings or Emails</small></h1>			</div>';		$this->folders2array();		$this->files2array();		$this->filearray2list();
+		
+	}	function folderCreate() {
+		// aktueller Folder? jahr(2) monat
+		$path = "files/" . substr( date("Y"), -2 ) . date("m");
+		// wenn nicht vorhanden, erstellen
+		is_dir($path) || mkdir($path);
+		return $path;
+	}
+	function folders2array() {		$folders = glob( $_SESSION['cfg_rootdir'] . '/files/*' );		foreach( $folders as $folder ) {			$this->folders[] = substr_replace( $folder, "", 0, ( strlen( $_SESSION['cfg_rootdir'] ) + 1 ) );		}	}	function files2array() {		$i = 0;		foreach( $this->folders as $folder ) {			$files = glob( $_SESSION['cfg_rootdir'] . '/' . $folder . '/*' );			// Verzeichnis abziehen			foreach( $files as $file ) {				$this->files[$i][] = substr_replace( $file, "", 0, ( strlen( $_SESSION['cfg_rootdir'] . '/' . $folder ) + 1 ) );				$this->files[$i][] = substr_replace( $file, "", 0, ( strlen( $_SESSION['cfg_rootdir'] ) + 1 ) );				$i++;			}		}	}	function filearray2list () {		echo '<div class="list-group">';		foreach( $this->files as $file ) {			echo '<a href="' . $file[1] . '" target="_blank" class="list-group-item">' . $file[0] . '<span class="badge">' . $this->human_filesize( filesize( $file[1] ), 1 ) . '</span></a>';		}		echo '</div>';	}	function human_filesize($bytes, $decimals = 2) {		$sz = 'BKMGTP';		$factor = floor((strlen($bytes) - 1) / 3);		return sprintf("%.{$decimals}f", $bytes / pow(1024, $factor)) . @$sz[$factor];	}
+	function uploadSimple() {
+		if ( is_array( $_FILES ) ) {
+			$this->fileUpload();
+		}
+//		print_r( $_POST );
+//		print_r( $_FILES );
+/*
+				<div class="form-group">
+					<label class="control-label">Select File</label>
+					<input type="file" class="btn btn-file" name="datei" role="file" >
+				</div>
+*/
+
+		echo '<form class="form-inline" action="' . $_SERVER['REQUEST_URI'] . '" method="post" name="files" enctype="multipart/form-data">
+				<div style="position:relative;">
+      			<a class="btn btn-primary" href=\'javascript:;\'>
+					Choose File...
+					<input type="file" style=\'position:absolute;z-index:2;top:0;left:0;filter: alpha(opacity=0);-ms-filter:"progid:DXImageTransform.Microsoft.Alpha(Opacity=0)";opacity:0;background-color:transparent;color:transparent;\' name="datei" size="40" onchange=\'$("#upload-file-info").html($(this).val());\'>
+					</a>&nbsp;
+					<span class="label label-info" id="upload-file-info"></span>
+				</div>
+				<p></p>
+				<button type="submit" class="btn btn-default" name="uploadBtn" id="submit-all">Submit all files</button>
+				</form>
+				<p></p>';
+	}
+	function fileUpload() {
+		$path = $this->folderCreate();
+		if ( !empty( $_FILES['datei']['name'] ) ) {
+//			$fileSub = substr (strrchr ($_FILES['datei']['name'], "."), 0);
+			if( $_FILES['datei']['size'] < 15360000 AND $_FILES['datei']['size'] > 0 ) {
+				// Datei kopieren
+				move_uploaded_file($_FILES['datei']['tmp_name'], $path . "/" . urlencode( $_FILES['datei']['name'] ) );
+			}
+		}
+	}
+	function uploadDropzone() {
+		print_r( $_POST );
+		print_r( $_FILES );
+		echo '<div class="page-header">				<h1>Upload file...</h1>			</div>
+			<div>
+			<meta charset="utf-8">
+
+<title>Dropzone simple example</title>
+
+
+<!--
+  DO NOT SIMPLY COPY THOSE LINES. Download the JS and CSS files from the
+  latest release (https://github.com/enyo/dropzone/releases/latest), and
+  host them yourself!
+-->
+<script src="dropzone/dist/dropzone.js"></script>
+<link rel="stylesheet" href="dropzone/dist/dropzone.css">
+
+
+<p>
+  This is the most minimal example of Dropzone. The upload in this example
+  doesn\'t work, because there is no actual server to handle the file upload.
+</p>
+
+<!-- Change /upload-target to your upload address -->
+<form action="files/1512" class="dropzone">
+
+</form>
+<button type="submit" id="submit-all">Submit all files</button>
+
+			</div>
+<script>
+Dropzone.options.myDropzone = {
+
+  // Prevents Dropzone from uploading dropped files immediately
+  autoProcessQueue: false,
+
+  init: function() {
+    var submitButton = document.querySelector("#submit-all")
+        myDropzone = this; // closure
+
+    submitButton.addEventListener("click", function() {
+      myDropzone.processQueue(); // Tell Dropzone to process all queued files.
+    });
+
+    // You might want to show the submit button only when 
+    // files are dropped here:
+    this.on("addedfile", function() {
+      // Show submit button here and/or inform user to click it.
+    });
+
+  }
+};
+</script>			
+			';
+	}}?>
