3d1ec33a568cddf267b9f451163fd47ffd82ba5e
Sven Anacker <sven@anacker.info>
2016-08-17 06:58:13 +0200

    Solved problem with short running scripts

--- inc/file_op.inc.php
+++ inc/file_op.inc.php
@@ -12,10 +12,7 @@
 	}
 	function folders2array() {		$folders = glob( $_SESSION['cfg_rootdir'] . '/files/*' );		foreach( $folders as $folder ) {			$this->folders[] = substr_replace( $folder, "", 0, ( strlen( $_SESSION['cfg_rootdir'] ) + 1 ) );		}	}	function files2array() {		$i = 0;		foreach( $this->folders as $folder ) {			$files = glob( $_SESSION['cfg_rootdir'] . '/' . $folder . '/*' );			// Verzeichnis abziehen			foreach( $files as $file ) {				$this->files[$i][] = substr_replace( $file, "", 0, ( strlen( $_SESSION['cfg_rootdir'] . '/' . $folder ) + 1 ) );				$this->files[$i][] = substr_replace( $file, "", 0, ( strlen( $_SESSION['cfg_rootdir'] ) + 1 ) );				$i++;			}		}	}	function filearray2list () {		echo '<div class="list-group">';		foreach( $this->files as $file ) {			echo '<a href="' . $file[1] . '" target="_blank" class="list-group-item">' . $file[0] . '<span class="badge">' . $this->human_filesize( filesize( $file[1] ), 1 ) . '</span></a>';		}		echo '</div>';	}	function human_filesize($bytes, $decimals = 2) {		$sz = 'BKMGTP';		$factor = floor((strlen($bytes) - 1) / 3);		return sprintf("%.{$decimals}f", $bytes / pow(1024, $factor)) . @$sz[$factor];	}
 	function uploadSimple() {
-		if ( is_array( $_FILES ) ) {
-			$this->fileUpload();
-		}
-//		print_r( $_POST );
+//		print_r( $_POST );
 //		print_r( $_FILES );
 /*
 				<div class="form-group">
@@ -23,7 +20,7 @@
 					<input type="file" class="btn btn-file" name="datei" role="file" >
 				</div>
 */
-
+/*
 		echo '<form class="form-inline" action="' . $_SERVER['REQUEST_URI'] . '" method="post" name="files" enctype="multipart/form-data">
 				<div style="position:relative;">
       			<a class="btn btn-primary" href=\'javascript:;\'>
@@ -40,15 +37,46 @@
 				<p>Unser Hoster 1&1 beschränkt die Ausführzeit für Scripte auf 60 Sekunden. Da der Upload größerer Dateien länger dauert,
 				bricht der Upload nach 60 Sekunden ab.</p>
 				<p>Versuche haben ergeben, das zum Beispiel 3 MB große Dateien funktionieren (wohl auch einiges mehr), 20 MB aber nicht!</p>
-				<p>Dies ist aber auch abhängig von der Internetverbindung. Mit einer vDSL Anbindung funktionieren unter Umständen auch 20 MB!</p>';
+				<p>Dies ist aber auch abhängig von der Internetverbindung. Mit einer vDSL Anbindung funktionieren unter Umständen auch 20 MB!</p>
+				';
+        <br/>
+        <label>Select File:</label>
+        <input type="file" name="file2" />
+*/
+		echo '<form onsubmit="getProgress()" target="_self" enctype="multipart/form-data" method="post">
+					<input type="hidden" name="UPLOAD_IDENTIFIER" value="<?php echo $id;?>" />
+					<label>Select File:</label>
+					<input type="file" name="file" />
+					<br/>
+					<label>Upload File:</label>
+					<input id="submitButton" type="submit" value="Upload File" />
+				</form>				
+				';
+//		var_dump($_FILES);
+//		print_r( $_FILES );
+
+		if ( is_array( $_FILES ) ) {
+			$this->fileUpload();
+		}
+
 	}
 	function fileUpload() {
 		$path = $this->folderCreate();
+
+		if ( !empty( $_FILES['file']['name'] ) ) {
+			if( $_FILES['file']['size'] < 55360000 AND $_FILES['file']['size'] > 0 ) {
+				// Gibt es die Datei schon? Dann ein "(1)" anhängen
+				
+				// Datei kopieren
+				echo $_FILES['file']['name'] . " wurde kopiert!\n";
+				$_SESSION['message'][] = "Die Datei wurde kopiert.::0"; 
+				move_uploaded_file($_FILES['file']['tmp_name'], $path . "/" . $_FILES['file']['name'] );
+			}
+		}
+
 		if ( !empty( $_FILES['datei']['name'] ) ) {
-//			$fileSub = substr (strrchr ($_FILES['datei']['name'], "."), 0);
 			if( $_FILES['datei']['size'] < 15360000 AND $_FILES['datei']['size'] > 0 ) {
 				// Datei kopieren
-//				echo '<p>Kopiere Datei "' . $_FILES['datei']['tmp_name'] . '" nach "' . $path . "/" . urlencode( $_FILES['datei']['name'] ) . '"</p>';
 				move_uploaded_file($_FILES['datei']['tmp_name'], $path . "/" . urlencode( $_FILES['datei']['name'] ) );
 			}
 		}
